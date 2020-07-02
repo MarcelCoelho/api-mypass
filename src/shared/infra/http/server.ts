9057564be658f95abf78f0506a705be94bd3913a
=Marcel Coelho <marcelfillipe@hotmail.com>
2020-07-02 18:59:27 -0300

    configurando porta heroku

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -27,6 +27,4 @@ app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
   });
 });
 
-app.listen(3333, () => {
-  console.log('🚀 Server started on port 3333');
-});
+app.listen(process.env.PORT || 3000);
